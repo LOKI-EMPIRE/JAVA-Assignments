8dc3886bc92a58e7af72dce9eadd5f56fa3ecae4
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-09 17:59:42 +0530

    Create Person.java

---
+++ 2 - Day 15/Session 1/Practice_SpringBoot_One_to_One_Passport/model/Person.java
@@ -0,0 +1,77 @@
+package com.examly.springapp.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String dateOfBirth;
+    private String email;
+    private String phoneNumber;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    private Passport passport;
+
+    public Person(){}
+
+    public Person(String name, String dateOfBirth, String email, String phoneNumber, Passport passport) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.passport = passport;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }  
+
+}
