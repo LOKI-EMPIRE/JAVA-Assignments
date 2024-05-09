c89646f670cd9dfbae93a37e13729158c81eef9f
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-10 00:20:56 +0530

    Create ProjectService.java

---
+++ 2 - Day 15/Session 2/Practice_SpringBoot_Project_Task_One_To_Many/service/ProjectService.java
@@ -0,0 +1,10 @@
+package com.examly.springapp.service;
+
+import com.examly.springapp.model.Project;
+
+public interface ProjectService {
+
+    Project addS(Project p);
+    Project getByIdS(int id);
+    
+}
