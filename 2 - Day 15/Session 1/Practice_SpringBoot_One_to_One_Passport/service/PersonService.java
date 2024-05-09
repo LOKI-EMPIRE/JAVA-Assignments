6cfc575efae2ae95e2b9228291119af15ff82e80
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-09 18:01:56 +0530

    Create PersonService.java

---
+++ 2 - Day 15/Session 1/Practice_SpringBoot_One_to_One_Passport/service/PersonService.java
@@ -0,0 +1,35 @@
+package com.examly.springapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.Passport;
+import com.examly.springapp.model.Person;
+import com.examly.springapp.repository.PassportRepository;
+import com.examly.springapp.repository.PersonRepository;
+
+@Service
+public class PersonService{
+
+    @Autowired
+    private PersonRepository repop;
+
+    @Autowired 
+    private PassportRepository repo;
+
+    public Person addPS(long id,Person p){
+        if(repo.existsById(id)){
+            Passport p1 = repo.findById(id).get();
+            p.setPassport(p1);
+        }
+        return null;
+    }
+
+    public Person getSR(long id){
+        if(repop.existsById(id)){
+            return repop.findById(id).get();
+        }
+        return null;
+    }
+    
+}
