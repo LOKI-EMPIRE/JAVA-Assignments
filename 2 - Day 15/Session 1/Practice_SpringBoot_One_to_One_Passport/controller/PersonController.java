60887e7afc02d9bf31d58ca9922a0b4c8af808e5
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-09 18:04:03 +0530

    Update PersonController.java

--- 2 - Day 15/Session 1/Practice_SpringBoot_One_to_One_Passport/controller/PersonController.java
+++ 2 - Day 15/Session 1/Practice_SpringBoot_One_to_One_Passport/controller/PersonController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.examly.springapp.model.Passport;
 import com.examly.springapp.model.Person;
 import com.examly.springapp.service.PersonService;
 
