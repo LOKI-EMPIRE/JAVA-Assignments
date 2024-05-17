1f7504e134ad2c82dfbd3191ce9d648f8e7426c6
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-17 22:55:51 +0530

    Create index.js

---
+++ 3 - Day 22 /Session 3/Q2_Functions/index.js
@@ -0,0 +1,34 @@
+class Book{
+    constructor(title,author,price,quantity){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.quantity = quantity;
+       
+        this.getBookInfo = function(){
+            return "Title: "+this.title+", Author: "+this.author+", Price: $"+this.price+", Quantity: "+this.quantity;
+        }
+    }
+}
+ 
+inventory = [];
+ 
+function createBook(title,author,price,quantity){
+    return new Book(title,author,price,quantity);
+}
+ 
+function addBookToInventory(title,author,price,quantity){
+    inventory.push(createBook(title,author,price,quantity));
+}
+ 
+function displayInventory(){
+    inventory.forEach(b => console.log(b.getBookInfo()));
+}
+ 
+function calculateTotalValue(){
+    var total = 0;
+    inventory.forEach(b => total += (b.quantity * b.price));
+    return total;
+}
+ 
+module.exports = {Book, addBookToInventory, displayInventory, calculateTotalValue, createBook, inventory};
