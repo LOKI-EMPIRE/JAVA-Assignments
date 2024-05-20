97da1464f37389cdf3d14171734daaf527352b51
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-20 21:37:03 +0530

    Create script.ts

---
+++ 3 - Day 24/Session 2/Q3_Array/script.ts
@@ -0,0 +1,15 @@
+let fruits:string[]=[];
+
+function addFruit(){
+    let fruit:string = (<HTMLInputElement>document.getElementById("fruitInput")).value;
+    
+    if(!fruit){
+        document.getElementById("errorMessage").innerHTML="Fruit name is required.";
+        return;
+    }
+
+    fruits.push(fruit);
+    let strfruit:string = fruits.join(",");
+
+    document.getElementById("fruitList").innerHTML="Fruits: "+strfruit;
+}
