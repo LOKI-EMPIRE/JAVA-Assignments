5fbf2fb6dba281bd6e843d2bdcb2195c29a5ef56
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-23 01:33:06 +0530

    Create text-modifier.component.ts

---
+++ 3 - Day 26/Session 3/Q1_TwoWayBinding/text-modifier.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-modifier',
+  templateUrl: './text-modifier.component.html',
+  styleUrls: ['./text-modifier.component.css']
+})
+export class TextModifierComponent implements OnInit {
+
+  originalText:string = "";
+  modifiedText:string = "";
+
+  constructor() {}
+
+  applyUppercase(){
+    this.modifiedText = this.originalText.toUpperCase();
+  }
+
+  applyLowercase(){
+    this.modifiedText = this.originalText.toLowerCase();
+  }
+
+  applyReverse(){
+    this.modifiedText = this.originalText.split("").reverse().join("");
+  }
+
+  clearText(){
+    this.originalText = "";
+    this.modifiedText = "";
+  }
+
+  ngOnInit(): void {
+  }
+
+}
