d7a1ddac6f4e1f4143ded181a3a1f9733ae7bb1c
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-26 13:23:02 +0530

    Update text-formatter.pipes.ts

--- 3 - Day 27/Session 3/Q3_Pipes/text-formatter.pipes.ts
+++ 3 - Day 27/Session 3/Q3_Pipes/text-formatter.pipes.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TextFormatterPipe implements PipeTransform {
 
-  transform(inputText: string, selectedFormat:string): string {
+  transform(inputText: string, selectedFormat:string ,seperator:string=' '): string {
     if(selectedFormat == 'uppercase'){
       return inputText.toUpperCase();
     }
@@ -15,21 +15,43 @@ export class TextFormatterPipe implements PipeTransform {
     }
 
     else if(selectedFormat == 'capitalize'){
-      let str:string[] = inputText.split(" ");
-      for(let s of str){
-        s.charAt(0).toUpperCase();
+      if(inputText.includes(" ")){
+        let str:string[] = inputText.split(seperator);
+        for(let i=0; i<str.length; i++){
+          str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+        }
+        return str.join(seperator);
       }
-      inputText = str.join(" ");
-      return inputText;
+      return inputText.charAt(0).toUpperCase() + inputText.slice(1);
     }
+    
 
     else if(selectedFormat == 'reverse'){
       return inputText.split("").reverse().join("");
     }
-    
+
     else{
       return inputText;
     }
   }
+
+  // constructor(private up:UpperCasePipe,
+  //   private low:LowerCasePipe,
+  //   private cap:TitleCasePipe
+  //   ){}
+ 
+  // transform(value: string,format:string): string {
+  //   switch(format){
+  //     case 'uppercase':
+  //       return this.up.transform(value);
+  //     case 'lowercase':
+  //       return this.low.transform(value);
+  //     case 'capitalize':
+  //       return this.cap.transform(value);
+  //     case 'reverse':
+  //       return value.split('').reverse().join('');
+  //     default:
+  //       return value;
+  //   }
 
 }
