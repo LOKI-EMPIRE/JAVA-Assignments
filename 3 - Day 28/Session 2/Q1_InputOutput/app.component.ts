80150a9416d1f2df2f6a68bc74b1b3e3ae8ce9f3
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-27 23:30:38 +0530

    Create app.component.ts

---
+++ 3 - Day 28/Session 2/Q1_InputOutput/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularapp';
+  tasks:string[]=[];
+ 
+  addTask(task){
+    this.tasks.push(task);
+  }
+ 
+  deleteTask(index:number){
+    this.tasks.splice(index,1);
+  }
+}
