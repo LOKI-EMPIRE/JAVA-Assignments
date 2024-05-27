80b1721fe603dcf0d5d9692f181b3f09790023b1
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-27 22:54:57 +0530

    Create vehicle.service.ts

---
+++ 3 - Day 30/Session 1/Q2_Services/vehicle.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehicleService {
+
+  vehicles:any[] = [];
+
+  constructor() { }
+
+  getVehicles():any[]{
+    return this.vehicles;
+  }
+
+  addVehicle(vehicle:any){
+    this.vehicles.push(vehicle);
+  }
+
+  deleteVehicle(index:number){
+    this.vehicles.splice(index,1);
+  }
+}
