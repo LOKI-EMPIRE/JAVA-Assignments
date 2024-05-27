dbf8afa0f3ce1344148a46270423d781d560fe8d
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-27 09:52:51 +0530

    Create book-view.component.ts

---
+++ 3 - Day 28/Session 1/Q3_Bookviewer/book-view.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { books } from 'src/books';
+
+@Component({
+  selector: 'app-bookview',
+  templateUrl: './bookview.component.html',
+  styleUrls: ['./bookview.component.css']
+})
+export class BookviewComponent implements OnInit {
+
+  bookId:number;
+  book:any;
+
+  constructor(private rt:Router,private activatedRoute:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.bookId = parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.book = books.find(b => b.id === this.bookId);
+  }
+
+ 
+}
