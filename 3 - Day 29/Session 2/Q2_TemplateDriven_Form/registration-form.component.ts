e2f4b1ad9a504df09fa52a6f9d8532d6798fd4c3
Lokeshwar P <98037256+LOKI-EMPIRE@users.noreply.github.com>
2024-05-28 17:55:12 +0530

    Update registration-form.component.ts

--- 3 - Day 29/Session 2/Q2_TemplateDriven_Form/registration-form.component.ts
+++ 3 - Day 29/Session 2/Q2_TemplateDriven_Form/registration-form.component.ts
@@ -1,28 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-@Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.css']
-})
-export class RegistrationFormComponent implements OnInit {
 
-  isSubmitted: boolean = false;
-  passwordMismatch: boolean = false;
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  user:any = { firstName: "",
+  lastName: "",
+  mobileNumber: "",
+  email: "",
+  password: "",
+  confirmPassword: ""
+  };
+
+  isFormSubmitted = false;
+
   constructor() { }
 
   ngOnInit(): void {
-
   }
 
-  onSubmit(form: NgForm) {
-    if(form.value.password !== form.value.confirmPassword){
-      this.passwordMismatch = true;
-    } else {
-      this.isSubmitted = true;
-      this.passwordMismatch = false;
-      form.reset();
+  onSubmit(registartionForm:NgForm){
+    if(registartionForm.valid){
+      this.isFormSubmitted = true;
     }
+  }
+
+  resetForm(registartionForm:NgForm){
+    registartionForm.reset();
+    this.isFormSubmitted = false;
   }
 
 }
